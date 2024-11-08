672bf19ecdf1d0eb19aaa8c09c4f507c59e8f7e9
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-11-08 14:57:52 +0200

    Random Timer

---
+++ Reefers/src/component/data/RandomTimer.cs
@@ -0,0 +1,43 @@
+﻿using SerpentEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reefers;
+
+public class RandomTimer : Timer
+{
+    public int MinWaitTime { get; set; } = 1;
+    public int MaxWaitTime { get; set; } = 1;
+
+    public RandomTimer(int minWaitTime, int maxWaitTime) : base(new Random().Next(minWaitTime, maxWaitTime + 1))
+    {
+        MaxWaitTime = maxWaitTime;
+        MinWaitTime = minWaitTime;
+    }
+
+    public void Start(int minWaitTime, int maxWaitTime)
+    {
+        MaxWaitTime = maxWaitTime;
+        MinWaitTime = minWaitTime;
+        Start(new Random().Next(MinWaitTime, MaxWaitTime + 1));
+    }
+
+    public override void End()
+    {
+        InvokeTimeout();
+        Time = 0;
+        Enabled = false;
+
+
+        if (Autostart)
+        {
+            Enabled = true;
+            Start(MinWaitTime, MaxWaitTime);
+        }
+
+    }
+
+}
